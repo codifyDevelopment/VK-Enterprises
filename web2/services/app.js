f224303f7ace3f587a2e013cb98851fa2e8fb8c1
ShadeenDas <88702352+ShadeenDas@users.noreply.github.com>
2023-03-05 22:30:36 +0530

    Add files via upload

---
+++ web2/services/app.js
@@ -0,0 +1,12 @@
+const inquiry_btn = document.getElementById('inquiry-btn');
+
+inquiry_btn.addEventListener('click', (e)=>{
+    
+console.log('clicked')
+
+});
+const newBtn = document.getElementByID('new-btn');
+
+newBtn.addEventListener('click', (e)=>{
+    console.log('clicked')
+})
