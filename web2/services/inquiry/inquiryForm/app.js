744c7dcad16298f6af6c0dd840f940425dd9c5cd
ShadeenDas <88702352+ShadeenDas@users.noreply.github.com>
2023-03-05 22:31:35 +0530

    Delete web2 directory

--- web2/services/inquiry/inquiryForm/app.js
+++
@@ -1,8 +0,0 @@
-
-
-const dateTimeInput = document.getElementById("date-time-input");
-const currentDate = new Date();
-const dateString = currentDate.toLocaleDateString();
-const timeString = currentDate.toLocaleTimeString();
-const dateTimeString = `${dateString}, ${timeString}`;
-dateTimeInput.setAttribute("value", `${dateTimeString}`);
